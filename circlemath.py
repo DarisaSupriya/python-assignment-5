22c0364dacbcc1af5a93386d7b2ac7f3e9044fd0
DarisaSupriya <67041614+DarisaSupriya@users.noreply.github.com>
2020-06-24 13:03:18 +0530

    Add files via upload

---
+++ circlemath.py
@@ -0,0 +1,6 @@
+import math
+
+r= float(input('enter the radius of the circlr:'))
+area = math.pi * r * r
+
+print('are of the circle is:%.2f'%area)
---
+++ trigonometry.py
@@ -0,0 +1,15 @@
+import math
+pi=3.14
+a=math.sin(60)
+print(a)
+b=math.cos(pi)
+print(b)
+c=math.tan(90)
+print(c)
+print('the value of 5^8:' + str(math.pow(5,8)))
+x=math.sqrt(400)
+print(x)
+y=math.ceil(23.56)
+print(y)
+z=math.floor(23.56)
+print(z)
